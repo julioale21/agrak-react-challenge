a195b896d0f9b0ccbfe3435799ad044cce02bf8c
Julio Romero <julio.romero@go1.com>
2023-02-08 10:50:01 -0300

    chore: fixed eslint issues

--- src/redux/slices/basicSlice.ts
+++ src/redux/slices/basicSlice.ts
@@ -1,19 +1,19 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { RootState } from "../store";
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit'
+import { type RootState } from '../store'
 
 export const basicSlice = createSlice({
-  name: "basic",
+  name: 'basic',
   initialState: {
-    message: "Happy Coding! 😋",
+    message: 'Happy Coding! 😋'
   },
   reducers: {
     changeMessage: (state, payload: PayloadAction<{ message: string }>) => {
-      state.message = payload.payload.message;
-    },
-  },
-});
+      state.message = payload.payload.message
+    }
+  }
+})
 
-export const { changeMessage } = basicSlice.actions;
+export const { changeMessage } = basicSlice.actions
 
-export const selectMessage = (state: RootState) => state.basic.message;
-export default basicSlice.reducer;
+export const selectMessage = (state: RootState): string => state.basic.message
+export default basicSlice.reducer
