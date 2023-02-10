4f2cec87b8f4718d4736ca9759e84d0289f03abb
Julio Romero <julio.romero@go1.com>
2023-02-09 23:12:16 -0300

    added basic logic to show user list

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,6 +1,5 @@
 import { type User } from "../interfaces/User";
-
-import axios from "axios";
+import axios, { type AxiosResponse } from "axios";
 
 const baseUrl = "https://635017b9df22c2af7b630c3e.mockapi.io/api/v1";
 
@@ -9,8 +8,18 @@ const api = axios.create({
 });
 
 export const fetchUsers = async (): Promise<User[]> => {
-  const response = await api.get("/users");
+  const response: AxiosResponse = await api.get("/users");
   const users: User[] = response.data;
 
   return users;
+};
+
+export const deleteUser = async (id: string): Promise<number | Error> => {
+  try {
+    const response: AxiosResponse = await api.post(`/users/${id}`);
+
+    return response.status;
+  } catch (error) {
+    return error as Error;
+  }
 };
