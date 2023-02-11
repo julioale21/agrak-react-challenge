fd466a790bc4c72b5ae15a1442478f13de83dee5
Julio Romero <julio.romero@go1.com>
2023-02-10 21:57:15 -0300

    chore: updated user.service, added update and fetch by id

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -5,6 +5,13 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
+export const fetchUserByID = async (id: string): Promise<User> => {
+  const response: AxiosResponse = await api.get(`/users/${id}`);
+  const user: User = response.data;
+
+  return user;
+};
+
 export const fetchUsers = async (): Promise<User[]> => {
   const response: AxiosResponse = await api.get("/users");
   const users: User[] = response.data;
@@ -12,21 +19,31 @@ export const fetchUsers = async (): Promise<User[]> => {
   return users;
 };
 
-export const deleteUser = async (id: string): Promise<number | Error> => {
-  try {
-    const response: AxiosResponse = await api.delete(`/users/${id}`);
-
-    return response.status;
-  } catch (error) {
-    return error as Error;
-  }
-};
-
 export const createUser = async (user: User): Promise<User | Error> => {
   try {
     const response: AxiosResponse = await api.post(`/users`, user);
 
     return response.data;
+  } catch (error) {
+    return error as Error;
+  }
+};
+
+export const updateUser = async (user: User): Promise<User | Error> => {
+  try {
+    const response: AxiosResponse = await api.put(`/users/${user.id as string}`, user);
+
+    return response.data;
+  } catch (error) {
+    return error as Error;
+  }
+};
+
+export const deleteUser = async (id: string): Promise<number | Error> => {
+  try {
+    const response: AxiosResponse = await api.delete(`/users/${id}`);
+
+    return response.status;
   } catch (error) {
     return error as Error;
   }
