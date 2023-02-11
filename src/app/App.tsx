efc7d19275d9526ab88933fa3487c4b4c1e04851
Julio Romero <julio.romero@go1.com>
2023-02-10 21:17:40 -0300

    added basic router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,18 @@
 import * as React from "react";
+// import { UserForm } from "../components/forms/UserForm";
 import UserListView from "../pages/UserListView";
+import UserFormView from "../pages/UserFormView";
 
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App(): JSX.Element {
   return (
-    <div>
-      <UserListView />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<UserListView />} path="/" />
+        <Route element={<UserFormView />} path="/user/:id" />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
