7c25fccfd3dae55d89da2d3230143431106ba045
Julio Romero <julio.romero@go1.com>
2023-02-11 16:38:32 -0300

    feature: created BasicLayout component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,19 @@
 import * as React from "react";
-// import { UserForm } from "../components/forms/UserForm";
 import UserListView from "../pages/UserListView";
-import UserFormView from "../pages/UserFormView";
+import UserManagementView from "../pages/UserManagementView";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App(): JSX.Element {
+const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<UserListView />} path="/" />
-        <Route element={<UserFormView />} path="/user/:id" />
+        <Route element={<UserManagementView />} path="/user" />
+        <Route element={<UserManagementView />} path="/user/:id" />
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
